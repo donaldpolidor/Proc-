02f1ad3f4e8ae2565cd6a627e75b3ecc5d082f08
do-3 <donaldpolidor30@github.com>
2024-04-27 15:47:57 -0400

    finished C# prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,43 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        int number;
+        do
+        {
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+            numbers.Add(number);
+        } while (number != 0);
+
+        // Basic Requirements
+        int sum = numbers.Sum();
+        double average = numbers.Average();
+        int max = numbers.Max();
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {max}");
+
+        // Stretch Challenge
+        List<int> positiveNumbers = numbers.Where(x => x > 0).ToList();
+        int minPositive = positiveNumbers.Min();
+
+        Console.WriteLine($"The smallest positive number is: {minPositive}");
+
+        numbers.Sort();
+        Console.WriteLine("The sorted list is:");
+        foreach (int num in numbers)
+        {
+            Console.WriteLine(num);
+        }
     }
 }
