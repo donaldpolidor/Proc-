b399abf41a97e5f2fdc7ae8ed4fe4d24cb4f6558
do-3 <donaldpolidor30@github.com>
2024-04-27 15:38:02 -0400

    finished C# prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        bool playAgain = false;
+
+        do
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+            int guess = -1;
+            int guessCount = 0;
+
+            Console.WriteLine("Welcome to Guess My Number!");
+            
+            while (guess != magicNumber)
+            {
+                Console.Write("Guess the magic number (between 1 and 100): ");
+                guess = int.Parse(Console.ReadLine());
+                guessCount++;
+
+                if (magicNumber > guess)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (magicNumber < guess)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine($"You guessed it in {guessCount} guesses!");
+                }
+            }
+
+            Console.Write("Do you want to play again? (yes/no): ");
+            string playAgainInput = Console.ReadLine().ToLower();
+            playAgain = playAgainInput == "yes" ? true : false;
+
+        } while (playAgain);
     }
 }
